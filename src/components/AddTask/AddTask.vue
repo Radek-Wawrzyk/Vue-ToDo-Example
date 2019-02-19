8244ca889ed5ebe2e1fb6e75f769b2517da918b2
Radek <radek511@op.pl>
2019-02-19 19:15:25 +0000

    Add - Simple animation for toggling input

--- src/components/AddTask/AddTask.vue
+++ src/components/AddTask/AddTask.vue
@@ -5,7 +5,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,11H13V5a1,1,0,0,0-2,0v6H5a1,1,0,0,0,0,2h6v6a1,1,0,0,0,2,0V13h6a1,1,0,0,0,0-2Z"/></svg>
       </button>
     </div>
-    <div class="add-task__input" v-if="edit">
+    <div class="add-task__input" :class="{'add-task__input--active' : edit}">
       <input :class="{'has-error' : error}" type="text" v-model="task.content" @keyup.enter="addTask" placeholder="Type your task" />
     </div>
   </div>
