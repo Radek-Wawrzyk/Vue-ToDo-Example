bcd91d1a196b7d30a4cc13fb0c2a1d5d416edfcf
Radek <radek511@op.pl>
2019-02-19 01:19:56 +0000

    Add - Item component with styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
-   <h1>s</h1>
+    <section class="task">
+      <ul class="tasks">
+        <task v-for="task in tasks" :key="task.id" :task="task" @deleteTask="deleteTask($event)" @changeStatus="changeStatus($event)" />
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
 
+import Task from './components/Task/Task.vue';
+
 export default {
   name: 'app',
+  components: {
+    Task
+  },
+  data: () => ({
+    tasks: [
+      {
+        id: 1,
+        status: false,
+        content: 'Kup mieszkanie i zabij michała i daj prace',
+        date: '10/20/2019'
+      },
+      {
+        id: 2,
+        status: true,
+        content: 'Naucz michała Vue.js!',
+        date: '12/20/2019'
+      }
+    ]
+  }),
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+    changeStatus(id) {
+
+      this.tasks.forEach(task => task.id === id ? task.status =! task.status : false);
+    }
+  }
 };
 
 </script>
+
+<style scoped src="./App.scss" lang="scss" />
---
+++ src/components/Task/Task.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="task">
+    <div class="task__input">
+      <button class="task__checkbox" @click="changeStatus(task.id)" :class="{'task__checkbox--done' : task.status}" >     
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.71,7.21a1,1,0,0,0-1.42,0L9.84,14.67,6.71,11.53A1,1,0,1,0,5.29,13l3.84,3.84a1,1,0,0,0,1.42,0l8.16-8.16A1,1,0,0,0,18.71,7.21Z"/></svg>
+      </button>
+    </div>
+    <p class="task__content" :class="{'task__content--done' : task.status}">{{task.content}}</p>
+    <span class="task__date" :class="{'task__date--done' : task.status}">{{task.date}}</span>
+    <button class="task__delete-btn" @click="deleteTask(task.id)">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"/></svg>
+    </button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'task',
+  props: {
+    task: Object
+  },
+  methods: {
+    deleteTask(id) {
+      this.$emit('deleteTask', id);
+    },
+    changeStatus(id) {
+      this.$emit('changeStatus', id);
+    }
+  }
+};
+
+</script>
+
+<style scoped src="./Task.scss" lang="scss" />
